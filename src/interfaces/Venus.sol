0de84979f72e063a6632b73c505961414b12e6d8
fmnxl <4031216+fmnxl@users.noreply.github.com>
2022-11-04 11:59:08 +0700

    set underlyingBalanceCurrent modifier

--- src/interfaces/Venus.sol
+++ src/interfaces/Venus.sol
@@ -10,7 +10,7 @@ interface IVToken is IERC20 {
   function redeemUnderlying(uint256 redeemAmount) external returns (uint256);
   function borrow(uint256 borrowAmount) external returns (uint256);
   function repayBorrow(uint256 repayAmount) external returns (uint256);
-  function balanceOfUnderlying(address owner) external view returns (uint256);
+  function balanceOfUnderlying(address owner) external returns (uint256);
   function borrowBalanceStored(address account) external view returns (uint256);
   function exchangeRateStored() external view returns (uint256);
   function borrowBalanceCurrent(address account) external returns (uint256);
